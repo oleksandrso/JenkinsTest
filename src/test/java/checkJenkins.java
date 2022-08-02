0a4c687eeccd5f05881f2a74121ddb76e9a2fae9
olexandrso <alex.worksot@gmail.com>
2022-08-02 17:22:35 +0300

    First

---
+++ src/test/java/checkJenkins.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class checkJenkins {
+    @Test
+    void test1() {
+        System.out.println("");
+        System.out.println("");
+        System.out.println("########ALL GOOD#######");
+        System.out.println("");
+        System.out.println("");
+    }
+}
