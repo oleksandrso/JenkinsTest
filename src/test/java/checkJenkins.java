a2b730570d1d85991b9e16c836e66b23ed07fe0e
olexandrso <alex.worksot@gmail.com>
2022-08-22 18:22:10 +0300

    next

--- src/test/java/checkJenkins.java
+++ src/test/java/checkJenkins.java
@@ -1,13 +1,29 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class checkJenkins {
     @Test
     void test1() {
-        System.out.println("");
-        System.out.println("");
-        System.out.println("########ALL GOOD#######");
-        System.out.println("");
-        System.out.println("");
-      //Something
+       assertTrue(true);
+       assertTrue(true);
+       assertTrue(true);
+    } @Test
+    void test2() {
+       assertTrue(true);
+       assertTrue(true);
+       assertTrue(true);
+    } @Test
+    void test3() {
+       assertTrue(true);
+       assertTrue(true);
+       assertTrue(true);
+    }
+
+    @Test
+    void test4() {
+        assertTrue(true);
+        assertTrue(true);
+        assertTrue(true);
     }
 }
