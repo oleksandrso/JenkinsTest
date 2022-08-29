612144d956e2c5bd22ec984e7d1a14df50d12df7
olexandrso <alex.worksot@gmail.com>
2022-08-29 11:42:11 +0300

    Change

--- src/test/java/tests/simple/PositiveTests.java
+++ src/test/java/tests/simple/PositiveTests.java
@@ -1,43 +1,57 @@
+package tests.simple;
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class checkJenkins {
+public class PositiveTests {
+    @Test
+    void test0() {
+        assertTrue(true);
+    }
+
     @Test
     void test1() {
         assertTrue(true);
-
     }
 
     @Test
     void test2() {
         assertTrue(true);
-
     }
 
     @Test
     void test3() {
         assertTrue(true);
-
     }
 
     @Test
     void test4() {
         assertTrue(true);
-
     }
 
     @Test
-    void testBlocked() {
+    void test5() {
         assertTrue(true);
-
     }
 
     @Test
-    void testBlocked2() {
+    void test6() {
         assertTrue(true);
-
     }
 
+    @Test
+    void test7() {
+        assertTrue(true);
+    }
 
+    @Test
+    void test8() {
+        assertTrue(true);
+    }
+
+    @Test
+    void test9() {
+        assertTrue(true);
+    }
 }
