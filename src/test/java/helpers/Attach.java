15da91d458e430110695f29b25d856e87216cb02
olexandrso <alex.worksot@gmail.com>
2022-08-29 12:16:34 +0300

    videoUrl

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -45,7 +45,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl(String sessionId) {
-        String videoUrl = "https://http://192.168.0.104:4444/video/" + sessionId + ".mp4";
+        String videoUrl = "http://192.168.0.104:4444/video/" + sessionId + ".mp4";
 
         try {
             return new URL(videoUrl);
