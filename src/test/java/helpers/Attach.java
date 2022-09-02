597c227892412001fb3e075d229122f752e3cccf
olexandrso <alex.worksot@gmail.com>
2022-09-02 13:50:47 +0300

    fix

--- src/test/java/helpers/Attach.java
+++ src/test/java/helpers/Attach.java
@@ -45,7 +45,7 @@ public class Attach {
     }
 
     public static URL getVideoUrl(String sessionId) {
-        String videoUrl = "http://192.168.0.1111:8080/video/" + sessionId + ".mp4";
+        String videoUrl = "http://localhost:4444/video/" + sessionId + ".mp4";
 
         try {
             return new URL(videoUrl);
