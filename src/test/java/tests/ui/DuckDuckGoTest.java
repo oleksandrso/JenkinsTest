1f71f93e7a855e2f997d6dbc6b057ace4fc7cd8a
olexandrso <alex.worksot@gmail.com>
2023-06-19 20:17:59 +0300

    1234

--- src/test/java/tests/ui/DuckDuckGoTest.java
+++ src/test/java/tests/ui/DuckDuckGoTest.java
@@ -1,5 +1,6 @@
 package tests.ui;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -7,6 +8,7 @@ import static com.codeborne.selenide.Selenide.sleep;
 
 public class DuckDuckGoTest extends TestBase{
     @Test
+    @Tag("simpleTests")
     void duckDuckGoOpens() {
         open("https://duckduckgo.com");
         sleep(100000);
