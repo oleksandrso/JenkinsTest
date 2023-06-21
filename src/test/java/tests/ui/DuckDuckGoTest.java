cb022313d25285a3addd635a786122d4d161ff25
olexandrso <alex.worksot@gmail.com>
2023-06-21 18:55:41 +0300

    allure

--- src/test/java/tests/ui/DuckDuckGoTest.java
+++ src/test/java/tests/ui/DuckDuckGoTest.java
@@ -3,14 +3,24 @@ package tests.ui;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Selenide.open;
-import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.*;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class DuckDuckGoTest extends TestBase{
     @Test
     void duckDuckGoOpens1() {
         open("https://duckduckgo.com");
-        sleep(15000);
+        assertTrue(true);
+    }
+    @Test
+    void duckDuckGoOpens2() {
+        open("https://duckduckgo.com");
+        assertTrue(true);
+    }
+    @Test
+    void duckDuckGoOpens3() {
+        open("https://duckduckgo.com");
+        assertTrue(false);
     }
 }
